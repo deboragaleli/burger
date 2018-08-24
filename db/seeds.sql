772edc256d0a4cddc6ecfeffe1fc32295dd6f4db
Debora Galeli <deboragaleli@gmail.com>
2018-08-23 23:09:21 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Barbecue Burger', TRUE);
+
+SELECT * FROM burgers;
